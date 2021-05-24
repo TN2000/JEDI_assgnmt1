e197685b37e631dd3fdc2ab9203ec5c0d28835c2
adivish1999 <40515546+adivish1999@users.noreply.github.com>
2021-05-24 22:10:49 +0530

    Add files via upload

--- Java/src/com/flipkart/bean/Student.java
+++ Java/src/com/flipkart/bean/Student.java
@@ -26,7 +26,7 @@ public class Student extends User
 	/**
 	 * @param department the department to set
 	 */
-	public void setDepartment(String department) 
+	public void setD(String department) 
 	{
 		this.department = department;
 	}
